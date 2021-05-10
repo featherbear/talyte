5d4d67c2e41700cad39cb9cb896a923bc7fd74af
Andrew Wong <andrew.j.wong@outlook.com>
2021-05-10 21:39:13 +1000

    Add idle text to tally screen
    Could say standby, but some people say "Standby camera 4" to mean they're on preview... 🙋‍♂️

--- src/standalone/views/TallyView.cpp
+++ src/standalone/views/TallyView.cpp
@@ -14,12 +14,20 @@ ViewInterface Tally = {
             Title("\n\nTalyte is not initialised");
             return;
         }
+        bool programLinked = !(talyteState->linkedProgram.isEmpty());
 
-        if (talyteState->linkedProgram == talyteState->currentProgram) {
-            M5.Lcd.fillScreen(RED);
-        } else if (talyteState->linkedProgram == talyteState->currentPreview) {
-            M5.Lcd.fillScreen(GREEN);
+        if (programLinked) {
+            if (talyteState->linkedProgram == talyteState->currentProgram) {
+                M5.Lcd.fillScreen(RED);
+                return;
+            } else if (talyteState->linkedProgram == talyteState->currentPreview) {
+                M5.Lcd.fillScreen(GREEN);
+                return;
+            }
         }
+        M5.Lcd.fillScreen(BLACK);
+        M5.Lcd.setTextDatum(CC_DATUM);
+        M5.Lcd.drawString("IDLE", M5.Lcd.width() / 2, M5.Lcd.height() / 2, 1);
 
         return; },
     .handleAltButtonPress = [](bool longPress) {},
