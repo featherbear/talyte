62b065a54f27c73ec5a379842a6b38db046b8aaa
Andrew Wong <andrew.j.wong@outlook.com>
2021-07-17 03:00:37 +1000

    Remove service worker

--- http/rollup.config.js
+++ http/rollup.config.js
@@ -111,25 +111,5 @@ export default {
 		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
 		preserveEntrySignatures: 'strict',
 		onwarn,
-	},
-
-	serviceworker: {
-		input: config.serviceworker.input().replace(/\.js$/, '.ts'),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				preventAssignment: true,
-				values:{
-					'process.browser': true,
-					'process.env.NODE_ENV': JSON.stringify(mode)
-				},
-			}),
-			commonjs(),
-			typescript({ sourceMap: dev }),
-			!dev && terser()
-		],
-		preserveEntrySignatures: false,
-		onwarn,
 	}
 };
