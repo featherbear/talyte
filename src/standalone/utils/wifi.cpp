31c6d12a3a8899dbc4f311f87563b04d6add9154
Andrew Wong <andrew.j.wong@outlook.com>
2021-07-17 00:27:55 +1000

    Add discovery function

--- src/standalone/utils/wifi.cpp
+++ src/standalone/utils/wifi.cpp
@@ -1,6 +1,12 @@
 #include "wifi.hpp"
 
+#include <ArduinoJson.h>
+
 namespace WifiUtils {
+
+String lastScanResult = "[]";
+uint32_t lastScanFinishedTime = 0;
+bool isScanning = false;
 
 void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
     Serial.println("Disconnected from WiFi access point");
@@ -33,6 +39,40 @@ void initWiFi(
     WiFi.begin(ssid, password);
 }
 
+String discovery() {
+    // If already scanning, ignore
+    if (isScanning) return lastScanResult;
+
+    // If last scan was performed less than 5 seconds ago, ignore
+    if (millis() - lastScanFinishedTime < 5000) return lastScanResult;
+
+    isScanning = true;
+
+    int n_networks = WiFi.scanNetworks();
+    Serial.printf("Found %d networks\n", n_networks);
+
+    // Calculate size
+    uint32_t docSize = 0;
+    for (int i = 0; i < n_networks; i++) {
+        docSize += 64 + 4;  // 64 - structure, 4 - 4-byte length
+        docSize += WiFi.SSID(i).length();
+        docSize += WiFi.BSSIDstr(i).length();
+    }
+
+    DynamicJsonDocument doc(docSize);
+    for (int i = 0; i < n_networks; i++) {
+        JsonArray apArray = doc.createNestedArray();
+        apArray.add(WiFi.SSID(i));
+        apArray.add(WiFi.BSSIDstr(i));
+        apArray.add(WiFi.RSSI(i));
+    }
+
+    serializeJson(doc, lastScanResult);
+    isScanning = false;
+    lastScanFinishedTime = millis();
+    return lastScanResult;
+}
+
 void startConfigurator() {
     WiFi.disconnect();
     WiFi.mode(WIFI_AP_STA);
@@ -42,6 +82,7 @@ void startConfigurator() {
     Serial.print("AP IP address: ");
     Serial.println(IP);
 
+    Serial.println(discovery());
     while (true) {
         delay(1000);
     }
